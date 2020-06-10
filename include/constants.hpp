cabd54aff6cccdd42b159b24622d7e2e1ae42ca7
Nikitylllka <you@gmail.com>
2020-06-10 10:17:43 +0300

    check4

--- include/constants.hpp
+++ include/constants.hpp
@@ -3,13 +3,14 @@
 // Created ...
 //
 
-#ifndef UNTITLED_CONSTANTS_H
+#ifndef INCLUDE_CONSTANTS_HPP_
 #define UNTITLED_CONSTANTS_H
-const char CHARACTERS[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const char CHARACTERS[] = "0123456789ABCDEFGHIJKLMNOPQR"
+"STUVWXYZabcdefghijklmnopqrstuvwxyz";
 static const size_t KEY_LENGTH = 1;
 static const size_t VALUE_LENGTH = 1;
 static const size_t FAMILY_NAME_LENGTH = 1;
 static const size_t FAMILY_COUNT = 3;
 static const size_t STR_COUNT = 5;
 static const size_t DEFAULT_THREAD_HASH = 2;
-#endif //UNTITLED_CONSTANTS_H
+#endif //INCLUDE_CONSTANTS_HPP_
